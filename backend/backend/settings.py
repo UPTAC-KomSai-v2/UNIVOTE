b9b0f5243c9da1409d248d97c5823cd5baf3a7be
Louie Constantine Opiniano <examp23.lo23@gmail.com>
2025-10-27 17:25:04 +0800

    add instructions for running

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'api'
 ]
+INSTALLED_APPS += ['corsheaders']
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True 
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
