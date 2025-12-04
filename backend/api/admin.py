89d11c7b35d2a911354a91295c01e8de07832dbd
unknown <raphaellizarde08@gmail.com>
2025-12-04 16:44:33 +0800

    make the manage profile for candidate read from the database

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+# 1. Make sure CandidateForPosition is imported here
+from .models import User, Election, CandidateProfile, Vote, Position, VoteReceipt, CandidateForPosition
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Election)
+admin.site.register(Position)
+admin.site.register(CandidateProfile)
+admin.site.register(Vote)
+admin.site.register(VoteReceipt)
+
+# 2. ADD THIS LINE to make it show up in the sidebar
+admin.site.register(CandidateForPosition)
