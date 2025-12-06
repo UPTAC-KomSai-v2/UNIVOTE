3ec0f346ad5a5b1367a4b8c4adee26a960e80c85
Jujusama <jgmacadine@up.edu.ph>
2025-12-07 01:49:39 +0800

    Added Placeholder page

--- frontend/src/pages/Placeholder/Placeholder.jsx
+++ frontend/src/pages/Placeholder/Placeholder.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./Placeholder.css";
 
 export default function Placeholder() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h2>Welcome!</h2>
-      <p>This is the placeholder page after login.</p>
+    <div class="container">
+      <div class="error_404_message">
+        <h2>Error 404 — Resource Not Found &#128279;&#10060;</h2>
+        <p>The server could not locate the requested URL.</p>
+      </div>
     </div>
   );
 }
